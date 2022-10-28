d0c20c0474e861a0b23f184f682726ed0ab0dc44
zebek95 <saguirrer.dev@gmail.com>
2022-10-28 11:54:29 -0500

    Add album model to order the code

---
+++ album.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+func getAlbums(c *gin.Context) {
+	c.JSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.JSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.JSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
